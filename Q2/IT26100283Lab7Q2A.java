e2ad22a859c97f00d1f9a285c2c52d5af5a9f20b
M.K.M.R.KHALEEL <it26100283@my.sliit.lk>
2026-09-14 13:23:17 +0530

    Lab7Q2

---
+++ Q2/IT26100283Lab7Q2A.java
@@ -0,0 +1,8 @@
+public class IT26100283Lab7Q2A{
+	public static void main(String[] args) {
+		System.out.println("$ $ $ $ $");
+		System.out.println("$ $ $ $ $");
+		System.out.println("$ $ $ $ $");
+		System.out.println("$ $ $ $ $");
+	}
+}
---
+++ Q2/IT26100283Lab7Q2C.java
@@ -0,0 +1,9 @@
+public class IT26100283Lab7Q2C{
+	public static void main(String[] args) {
+		System.out.println("55555");
+		System.out.println("4444");
+		System.out.println("333");
+		System.out.println("22");
+		System.out.println("1");
+	}
+}
